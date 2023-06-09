c88504d6aee501fb74cc273b0259805c998eb4d4
Sumeet Kumar <81513463+sumeetk10@users.noreply.github.com>
2023-06-09 13:00:40 +0530

    initial commit

---
+++ Easy/ClimbingStairs.java
@@ -0,0 +1,40 @@
+//      https://leetcode.com/problems/climbing-stairs/description/
+
+public class ClimbingStairs {
+    class Solution {
+        public int climbStairs(int n) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(1,1);
+            map.put(2,2);
+            return climbStairs(n, map);
+        }
+        int climbStairs(int n, HashMap<Integer,Integer> map){
+            if(map.containsKey(n)){
+                return map.get(n);
+            }
+            map.put(n,climbStairs(n-1,map) + climbStairs(n-2,map));
+            return map.get(n);
+        }
+    }
+}
+
+// TC = 0(n)
+// SC = 0(n)
+
+// Bottom up optimised
+
+class solution{
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1) return n;
+
+        int prev1 = 2;
+        int prev2 = 1;
+        int i = 0;
+        while(i>2 && i<=n){
+            int newValue = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = newValue;
+        }
+        return prev1;
+    }
+}
---
+++ Easy/StringComparison.java
@@ -0,0 +1,33 @@
+//      https://leetcode.com/problems/string-compression/description/
+
+public class StringComparison {
+    class Solution {
+        public int compress(char[] chars) {
+            int count = 1;
+            int index = 0;
+    
+            if(chars.length == 0){
+                return 0;
+            }
+    
+            for(int i=0; i<chars.length; i++){
+                count = 1;
+                while(i<chars.length-1 && chars[i] == chars[i+1]){
+                    count++;
+                    i++;
+                }
+                chars[index] = chars[i];
+                index++;
+                if(count>1){
+                    for(char c: Integer.toString(count).toCharArray()){
+                        chars[index] = c;
+                        index++;
+                    }
+                }
+            }
+    
+            return index;
+        }
+    }
+}
+
